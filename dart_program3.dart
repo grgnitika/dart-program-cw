563688db01b09a098d5bdd5b82e1bba4a40f3c30
grgnitika <152359506+grgnitika@users.noreply.github.com>
2024-11-19 09:06:14 +0545

    dart third program

---
+++ dart_program3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = -7; // Replace this with any number to test
+
+  if (number > 0) {
+    print("$number is a positive number.");
+  } else if (number < 0) {
+    print("$number is a negative number.");
+  } else {
+    print("The number is zero.");
+  }
+}
