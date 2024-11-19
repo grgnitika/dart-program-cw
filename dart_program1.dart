2192038bc50c156d3629790e169300f2877f5f86
grgnitika <152359506+grgnitika@users.noreply.github.com>
2024-11-19 08:59:56 +0545

    first dart program

---
+++ dart_program1.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Declare two numbers
+  int num1 = 8;
+  int num2 = 5;
+
+  // Perform multiplication
+  int result = num1 * num2;
+
+  // Print the result
+  print('The multiplication of $num1 and $num2 is $result');
+}
